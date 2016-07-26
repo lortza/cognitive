bdfe777b60fd1e43487e2d11f23c0886f6c90741
anne <richardson.ae@gmail.com>
2016-07-25 19:32:52 -0500

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+    def require_signin
+      unless current_user
+        session[:intended_url] = request.url
+        redirect_to new_session_url, notice: "Please sign in first!"
+      end
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end #current_user
+
+    #make this method available for use in views
+    helper_method :current_user
+
+    def current_user?(user)
+      current_user == user
+    end #current_user?(user)
+    helper_method :current_user?
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_admin, only: [:index]
+  before_action :require_correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -39,7 +42,9 @@ class UsersController < ApplicationController
 
   def create
      @user = User.new(user_params)
-     if @user.save
+     if @user.save && session[:user_id] != nil
+      redirect_to users_url
+     elsif @user.save
        session[:user_id] = @user.id
        redirect_to @user
      else
@@ -79,14 +84,33 @@ class UsersController < ApplicationController
   #   end
   # end
   def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    session[:user_id] = nil
-    redirect_to root_url, alert: @user.name + "'s account has been successfully deleted."
+    # @user = User.find(params[:id])
+    if current_user.admin == TRUE
+      @user.destroy
+      redirect_to users_path, alert: @user.name + "'s account has been successfully deleted."
+    else
+      @user.destroy
+      session[:user_id] = nil
+      redirect_to root_url, alert: @user.name + "'s account has been successfully deleted."
+    end
   end
 
 
   private
+
+    def require_admin
+      unless current_user.admin == TRUE
+        redirect_to root_url, notice: "Please sign in as an Admin User first!"
+      end
+    end
+
+    def require_correct_user
+      @user = User.find(params[:id])
+      unless current_user == @user || current_user.admin == TRUE
+        redirect_to root_url
+      end#unless
+    end #require_correct_user
+
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
