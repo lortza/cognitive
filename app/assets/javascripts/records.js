0ddd41d7f77c57a805ab0bc5b520f3eea11b76ed
anne <richardson.ae@gmail.com>
2016-12-29 10:05:31 -0600

    Refactor js into fewer steps

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -1,48 +1,46 @@
-document.addEventListener("turbolinks:change", function() {
+document.addEventListener("turbolinks:load", function() {
+
+  $('.progressbar').css("display", "block");
 
   $('p.step1-link').click(function(event){
     event.preventDefault();
-    $('#step0').css("display", "none");
+    $('.step').css("display", "none");
     $('#step1').css("display", "block");
-    $('.progressbar').css("display", "block");
-    $('#step2').css("display", "none");
+    $('.progress-block').css("width", "5%");
   });
 
   $('p.step2-link').click(function(event){
     event.preventDefault();
     $('#step1').css("display", "none");
+    $('.step').css("display", "none");
     $('#step2').css("display", "block");
     $('.progress-block').css("width", "25%");
-    $('#step3').css("display", "none");
   });
 
   $('p.step3-link').click(function(event){
     event.preventDefault();
-    $('#step2').css("display", "none");
+    $('.step').css("display", "none");
     $('#step3').css("display", "block");
-    $('.progress-block').css("width", "45%");
-    $('#step4').css("display", "none");
+    $('.progress-block').css("width", "50%");
   });
 
   $('p.step4-link').click(function(event){
     event.preventDefault();
-    $('#step3').css("display", "none");
+    $('.step').css("display", "none");
     $('#step4').css("display", "block");
-    $('.progress-block').css("width", "65%");
-    $('#step5').css("display", "none");
+    $('.progress-block').css("width", "66%");
   });
 
   $('p.step5-link').click(function(event){
     event.preventDefault();
-    $('#step4').css("display", "none");
+    $('.step').css("display", "none");
     $('#step5').css("display", "block");
-    $('.progress-block').css("width", "85%");
-    $('#step6').css("display", "none");
+    $('.progress-block').css("width", "83%");
   });
 
   $('p.step6-link').click(function(event){
     event.preventDefault();
-    $('#step5').css("display", "none");
+    $('.step').css("display", "none");
     $('#step6').css("display", "block");
     $('.progress-block').css("width", "99%");
   });
